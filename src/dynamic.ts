d6840596e8de3c749285cadefc735e6a286e7bde
すえ <souhait.0614@gmail.com>
2024-06-21 09:57:58 +0900

    試験的にdynamic import版を用意

---
+++ src/dynamic.ts
@@ -0,0 +1,43 @@
+/**
+ * @module
+ *
+ * submarin-converterのPluginとして設定することで文章をﾒﾝﾍﾗっぽぃ日本語に変換できます
+ *
+ * @example
+ * ```typescript
+ * import { Converter } from "@submarin-converter/core";
+ * import genhera from "@submarin-converter/plugin-genhera";
+ *
+ * const converter = new Converter({ genhera });
+ *
+ * const { text } = await converter.convert("メロスは激怒した。", ["genhera"]);
+ *
+ * console.log(text) // "ﾒﾛｽゎ激怒した。。。"
+ * ```
+ */
+
+import type { Plugin } from "@submarin-converter/core";
+
+const dynamicGenerate = async (text: string) => {
+  const { generate } = await import("genhera");
+  return generate(text);
+};
+const dynamicFallbackFunction = async (text: string) => {
+  const { fallbackFunction } = await import("./fallbackFunction.ts");
+  return fallbackFunction(text);
+};
+
+/** submarin-converterのPluginとして設定できるPlugin */
+const plugin: Plugin<undefined> = {
+  convertFunctions: [dynamicGenerate, dynamicFallbackFunction],
+  metaData: {
+    displayName: "メンヘラ",
+    description: "日本語をﾒﾝﾍﾗっぽぃ日本語に変換します",
+    homepage: "https://github.com/Submarinonline/genhera.js",
+    repository:
+      "https://github.com/souhait0614/submarin-converter-plugin-genhera",
+    author: "すえ",
+  },
+};
+
+export default plugin;
--- test/convert.test.ts
+++ test/convert.test.ts
@@ -3,6 +3,7 @@ import genhera from "../src/index.ts";
 import { assertEquals } from "@std/assert";
 import { fallbackFunction } from "../src/fallbackFunction.ts";
 import { generate } from "genhera";
+import genheraDynamic from "../src/dynamic.ts";
 
 Deno.test("convert", async () => {
   const converter = new Converter({ genhera });
@@ -22,6 +23,38 @@ Deno.test("convert function", async () => {
 Deno.test("fallback function", async () => {
   const plugin: Plugin<undefined> = {
     convertFunctions: [fallbackFunction],
+  };
+  const converter = new Converter({ plugin });
+  const { text } = await converter.convert("メロスは激怒した。", ["plugin"]);
+  assertEquals(text, "ﾒﾛｽゎ激怒した。。。");
+});
+
+Deno.test("dynamic convert", async () => {
+  const converter = new Converter({ genheraDynamic });
+  const { text } = await converter.convert("メロスは激怒した。", [
+    "genheraDynamic",
+  ]);
+  assertEquals(text, "ﾒﾛｽゎ激怒した。。。");
+});
+
+Deno.test("dynamic convert function", async () => {
+  const plugin: Plugin<undefined> = {
+    convertFunctions: [async (text: string) => {
+      const { generate } = await import("genhera");
+      return generate(text);
+    }],
+  };
+  const converter = new Converter({ plugin });
+  const { text } = await converter.convert("メロスは激怒した。", ["plugin"]);
+  assertEquals(text, "ﾒﾛｽゎ激怒した。。。");
+});
+
+Deno.test("dynamic fallback function", async () => {
+  const plugin: Plugin<undefined> = {
+    convertFunctions: [async (text: string) => {
+      const { fallbackFunction } = await import("../src/fallbackFunction.ts");
+      return fallbackFunction(text);
+    }],
   };
   const converter = new Converter({ plugin });
   const { text } = await converter.convert("メロスは激怒した。", ["plugin"]);
